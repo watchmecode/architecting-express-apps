228ac5746cc6b9b83e17fc938bbabf123908269e
Derick Bailey <derickbailey@gmail.com>
2015-09-06 17:52:30 -0500

    recorded episode 2

--- episode-2/media/app.js
+++ episode-2/media/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -23,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
